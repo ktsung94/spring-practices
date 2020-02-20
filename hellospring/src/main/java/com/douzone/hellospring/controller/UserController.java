165d2eade1a8fa003944905436f9a23bd39002dc
R35 <R35@DESKTOP-IL5J0O6>
2020-02-20 16:00:25 +0900

    init commit

---
+++ hellospring/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.douzone.hellospring.vo.UserVo;
+
+/*
+ * RequestMapping
+ * Class + Method
+ */
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	//UserRepository userRepository;
+	
+	@RequestMapping(value= {"/join", "j"}, method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@GetMapping(value= {"/join", "j"})
+	public String join2() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo userVo) {
+		System.out.println(userVo);
+		
+		return "redirect:/main";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/update")
+	public String update(@RequestParam("n") String name) {
+		/*
+		 * 만약 n에 파라미터가 없는 경우 400 bad request 오류가 발생
+		 */
+		System.out.println(name);
+		return "UserController:update";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/update2")
+	public String update2(
+			@RequestParam(value="n", required=false, defaultValue="") String name,
+			@RequestParam(value="a", required=false, defaultValue="0") int age) {
+		System.out.println("---" + name + "---" + age + "---");
+		return "UserController:update2";
+	}
+}
