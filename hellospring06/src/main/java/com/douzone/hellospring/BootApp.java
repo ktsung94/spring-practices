db0a25c45dce09591331e6d26bcde79a3a14b3de
R35 <R35@DESKTOP-IL5J0O6>
2020-03-26 16:19:43 +0900

    hellospring06 변경!

--- hellospring06/src/main/java/com/douzone/hellospring/BootApp.java
+++ hellospring06/src/main/java/com/douzone/hellospring/BootApp.java
@@ -3,13 +3,9 @@ package com.douzone.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootConfiguration
-//@EnableAutoConfiguration
-//@ComponentScan("com.douzone.springbootex.controller")
 @SpringBootApplication
 public class BootApp {
 	public static void main(String[] args) {
 		SpringApplication.run(BootApp.class, args);
 	}
-
 }
